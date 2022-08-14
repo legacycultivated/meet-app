1df7188f507b6715c8d22dd0924e8e150e227bde
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2022-08-14 15:59:21 -0700

    add new functions

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -89,6 +89,9 @@ module.exports.getAccessToken = async (event) => {
     .then((token) => {
       // Respond with OAuth token
       return {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
         statusCode: 200,
         body: JSON.stringify(token),
       };
@@ -97,8 +100,61 @@ module.exports.getAccessToken = async (event) => {
       // Handle error
       console.error(err);
       return {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
         statusCode: 500,
         body: JSON.stringify(err),
+      };
+    });
+};
+
+module.exports.getCalendarEvents = (event) => {
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+  // Decode authorization code extracted from the URL query
+  const access_token = decodeURIComponent(
+    `${event.pathParameters.access_token}`
+  );
+  oAuth2Client.setCredentials({ access_token });
+
+  return new Promise((resolve, reject) => {
+    calendar.events.list(
+      {
+        calendarId: calendar_id,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      },
+      (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      }
+    );
+  })
+    .then((results) => {
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({ events: results.data.items }),
+      };
+    })
+    .catch((error) => {
+      return {
+        statusCode: 500,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify(error),
       };
     });
 };
