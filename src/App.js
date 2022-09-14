6a94d70384153d0fc5541bd5a30b029d0b5673b7
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2022-09-13 21:54:44 -0700

    debug alert message

--- src/App.js
+++ src/App.js
@@ -144,7 +144,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Meet App</h1>
-        <WarningAlert text={warningText} />
+        <WarningAlert text={this.state.warningText} />
         <CitySearch
           locations={this.state.locations}
           updateEvents={this.updateEvents}
